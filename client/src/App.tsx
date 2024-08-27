148de3f9b4ec84670abf02733fef42167a540fbc
Aricoins <arielgarcia79@gmail.com>
2024-08-27 00:01:14 -0300

    feat: hooktheme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,44 @@
+// src/App.tsx
+import React from 'react';
+import useThemeSwitcher from './hooks/useThemeSwitcher';
+import styled from 'styled-components';
 
-function App() {
+const Container = styled.div`
+  background-color: ${(props) => props.theme.background};
+  color: ${(props) => props.theme.text};
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.3s ease-in-out;
+`;
 
+const Button = styled.button`
+  background-color: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.text};
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+  font-size: 16px;
+  border-radius: 5px;
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    background-color: ${(props) => props.theme.accent};
+  }
+`;
+
+const App: React.FC = () => {
+  const { themeMode, toggleTheme, ThemeProvider } = useThemeSwitcher();
 
   return (
-    <>
-    Proyecto No Country
-    </>
-  )
-}
+    <ThemeProvider theme={themeMode}>
+      <Container>
+        <h1>LAW APP</h1>
+        <Button onClick={toggleTheme}>Toggle Theme</Button>
+      </Container>
+    </ThemeProvider>
+  );
+};
 
-export default App
+export default App;
