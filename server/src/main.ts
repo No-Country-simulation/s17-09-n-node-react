0017ea61200500d1784e0c8f5f4bdc4ed59db89e
jp-quintana <juanquintana1996@gmail.com>
2024-08-28 16:28:32 -0300

    feat: add prisma setup

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,14 @@
 import App from './app'
+import { PrismaClient } from '@prisma/client'
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 async function main() {
+  const prisma = new PrismaClient()
+  await prisma.$connect()
   const app = new App().start()
   // eslint-disable-next-line no-console
   app.listen(3000, () => console.log('Listening on port 3000'))
+  await prisma.$disconnect()
 }
 
 void main()
