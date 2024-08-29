9dc70ac7e813d97c2fd5d4b8872fe1804867c1bd
schweigenderFlugel <facundoezcastro@gmail.com>
2024-08-29 00:30:59 -0300

    fix change port field to not required

--- server/src/config.ts
+++ server/src/config.ts
@@ -2,7 +2,7 @@ import env from 'env-var'
 
 export const envs = {
   nodeEnv: env.get('NODE_ENV').required().asString(),
-  port: env.get('PORT').required().asPortNumber(),
+  port: env.get('PORT').asPortNumber(),
   jwtAccessSecret: env.get('JWT_ACCESS_SECRET').asString(),
   jwtRefreshSecret: env.get('JWT_REFRESH_SECRET').asString(),
 }
