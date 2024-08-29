19d01d8c909c8010f953648c5bb4f03e94ad6c9e
Aricoins <arielgarcia79@gmail.com>
2024-08-28 21:13:51 -0300

    feat: landing login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,10 @@
 import React from 'react';
 import useThemeSwitcher from './hooks/useThemeSwitcher';
-import styled from 'styled-components';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './modules/auth/Sign-in';
 import LandingPage from './pages/LandingLogin'; // Asegúrate de que este componente esté correctamente importado
 
-const Container = styled.div`
-  background-color: ${(props) => props.theme.background};
-  color: ${(props) => props.theme.text};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  transition: all 0.3s ease-in-out;
-`;
 
-const Button = styled.button`
-  background-color: ${(props) => props.theme.primary};
-  color: ${(props) => props.theme.text};
-  border: none;
-  padding: 10px 20px;
-  cursor: pointer;
-  font-size: 16px;
-  border-radius: 5px;
-  transition: all 0.3s ease-in-out;
-
-  &:hover {
-    background-color: ${(props) => props.theme.accent};
-  }
-`;
 
 const App: React.FC = () => {
   const { themeMode, toggleTheme, ThemeProvider: CustomThemeProvider } = useThemeSwitcher();
