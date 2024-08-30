854c14a173f147ca29db15ef2ea3803004e0a7d6
schweigenderFlugel <facundoezcastro@gmail.com>
2024-08-30 18:59:49 -0300

    change validations rules in dtos and documentation

--- server/src/config/errors.ts
+++ server/src/config/errors.ts
@@ -1,12 +1,12 @@
 export default class HttpError {
   public status: number
   public message: string
-  public description: string | undefined
+  public description: string | string[] | undefined
 
   constructor(
     statusCode: number = 500,
     message: string,
-    description: string | undefined,
+    description: string | string[] | undefined,
   ) {
     this.status = statusCode
     this.message = message
--- server/src/dtos/user/login-dto.user.ts
+++ server/src/dtos/user/login-dto.user.ts
@@ -6,12 +6,33 @@ export class LoginUserDTO {
     public password: string,
   ) {}
 
-  static create(object: { [key: string]: string }): [string?, LoginUserDTO?] {
+  static create(object: { [key: string]: string }): [string[]?, LoginUserDTO?] {
     const { email, password } = object
+    const keys = ['email', 'password']
 
-    if (!email) return ['Missing email']
-    if (!Validators.email.test(email)) return ['Email is not valid']
-    if (!password) return ['Missing password']
+    if (
+      Object.values(object).length > LoginUserDTO.length ||
+      !keys.every((key) => Object.keys(object).includes(key)) ||
+      !Validators.email.test(email)
+    ) {
+      // eslint-disable-next-line prefer-const
+      let errors = []
+      if (!keys.every((key) => Object.keys(object).includes(key))) {
+        if (!email) errors.push(`Missing 'email'`)
+        if (!password) errors.push(`Missing 'password'`)
+        Object.keys(object)
+          .filter((key) => key !== 'email' && key !== 'password')
+          .forEach((key) => errors.push(`'${key}' should not exist`))
+      } else {
+        if (Object.values(object).length > LoginUserDTO.length)
+          Object.keys(object)
+            .filter((key) => key !== 'email' && key !== 'password')
+            .forEach((key) => errors.push(`'${key}' should not exist`))
+        if (!Validators.email.test(email))
+          errors.push(`'email' provided is not valid`)
+      }
+      return [errors]
+    }
 
     return [undefined, new LoginUserDTO(email, password)]
   }
