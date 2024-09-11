be15fed825a8aa2d3e09c952cb83bc7897025310
jumagu <jmgg.gutierrez@gmail.com>
2024-09-11 12:59:23 -0500

    refactor: Remove unnecessary headers from lawCaseApi.ts

--- client/src/apis/lawCaseApi.ts
+++ client/src/apis/lawCaseApi.ts
@@ -3,8 +3,4 @@ import axios from 'axios'
 export const lawCaseApi = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': true,
-  },
 })
