fbb26863690af90e826f493a6b629f1b8de15cb3
Facundo Castro <123614305+schweigenderFlugel@users.noreply.github.com>
2024-09-11 22:31:18 -0300

    Update lawCaseApi.ts

--- client/src/apis/lawCaseApi.ts
+++ client/src/apis/lawCaseApi.ts
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 export const lawCaseApi = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
   headers: {
-    'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Credentials': true,
+    'Access-Control-Allow-Origin': '*',
   },
+  withCredentials: true,
 })
