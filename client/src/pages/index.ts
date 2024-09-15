ee38ea2d8fd10582b20fdb46d4e70daea4e4b9c1
jumagu <jmgg.gutierrez@gmail.com>
2024-09-15 17:29:05 -0500

    Add NotFoundPage component

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,2 +1,4 @@
 export { default as Layout } from './Layout'
+export { default as HomePage } from './Home'
 export { default as HelpPage } from './HelpPage'
+export { default as NotFoundPage } from './NotFoundPage'
