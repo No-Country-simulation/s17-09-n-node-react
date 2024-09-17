979cf89e428ee92e79a77e0a1b2f89fde0db837b
samuelbernal44 <samuelbernal44@gmail.com>
2024-09-17 15:26:03 -0500

    chore(jest): configure Jest to handle TypeScript and ignore dist folder

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['**/test/**/*.spec.ts'], // Ejecuta solo archivos de prueba TypeScript
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest', // Asegúrate de usar ts-jest para compilar archivos .ts
+  },
+  testPathIgnorePatterns: ['dist/'], // Ignora la carpeta dist
 }
