1318fe6b0352b0eddb7b7425aa475799fbd5d102
jp-quintana <juanquintana1996@gmail.com>
2024-09-17 23:08:42 -0300

    fix: bug in get movements by user id and date service

--- server/src/services/movement.service.ts
+++ server/src/services/movement.service.ts
@@ -79,10 +79,10 @@ export class MovementService {
     }
 
     const startOfDay = new Date(date)
-    startOfDay.setHours(0, 0, 0, 0)
+    startOfDay.setUTCHours(0, 0, 0, 0)
 
     const endOfDay = new Date(date)
-    endOfDay.setHours(23, 59, 59, 999)
+    endOfDay.setUTCHours(23, 59, 59, 999)
 
     const movements = await prisma.movement.findMany({
       where: {
